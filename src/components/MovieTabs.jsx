d8abe23e85db41cfe0cd30967307baf6239bc0e0
Valeriy <waliush@mail.ru>
2020-04-04 17:24:29 +0300

    added master

---
+++ src/components/MovieTabs.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+
+class MovieTabs extends React.Component{
+  componentWillReceiveProps(nextProps, nextState) {
+    console.log("willReceiveProps");
+    console.log("nextProps sort_by", nextProps.sort_by);
+    console.log("prevProps sort_by", this.props.sort_by);
+  }
+  
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.sort_by !== this.props.sort_by) {
+       return true;
+    } else {
+      return false;
+    }
+  }
+  
+  render() {
+     const { sort_by, updateSortBy } = this.props;
+  // const handleClick = value => () => {
+  //     updateSortBy(value);
+  //   };
+  // };
+  const handleClick = value => {
+    return event => {
+      updateSortBy(value);
+    };
+  };
+
+  const getClassActiveLink = value => {
+    return `nav-link ${sort_by === value ? "active" : ""}`;
+  };
+
+  return (
+    <ul className="tabs nav nav-pills">
+      <li className="nav-item">
+        <div
+          className={getClassActiveLink("popularity.desc")}
+          onClick={handleClick("popularity.desc")}
+        >
+          Popularity desc
+        </div>
+      </li>
+      <li className="nav-item">
+        <div
+          className={getClassActiveLink("revenue.desc")}
+          // className={`nav-link ${sort_by === "revenue.desc" ? "active" : ""}`}
+          onClick={handleClick("revenue.desc")}
+        >
+          Revenue desc
+        </div>
+      </li>
+      <li className="nav-item">
+        <div
+          className={getClassActiveLink("vote_avetage.desc")}
+          // className={`nav-link ${sort_by === "vote_avetage.desc" ? "active" : ""}`}
+          onClick={handleClick("vote_avetage.desc")}
+        >
+          Vote avetage desc
+        </div>
+      </li>
+    </ul>
+  );
+ }
+
+}
+
+     
+
+export default MovieTabs;
