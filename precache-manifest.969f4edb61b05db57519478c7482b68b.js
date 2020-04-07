ce5ecd4285e6e803392c82a5e7888cacf346ec08
Valeriy <waliush@mail.ru>
2020-04-08 01:12:27 +0300

    Updates

---
+++ precache-manifest.969f4edb61b05db57519478c7482b68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51d354bd18b111c74381",
+    "url": "./static/css/main.8407a952.chunk.css"
+  },
+  {
+    "revision": "51d354bd18b111c74381",
+    "url": "./static/js/main.47ec5efe.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "02b0a59e4d384cdbc6a7",
+    "url": "./static/css/2.3ac6d48a.chunk.css"
+  },
+  {
+    "revision": "02b0a59e4d384cdbc6a7",
+    "url": "./static/js/2.1c8d0edb.chunk.js"
+  },
+  {
+    "revision": "71700c7356818b6e9a9345a1a1491b13",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactwarriors-master-class-master/precache-manifest.ffeaca0d7e278ab278a7c75cf6ad8723.js"
+  "./precache-manifest.969f4edb61b05db57519478c7482b68b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactwarriors-master-class-master/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
